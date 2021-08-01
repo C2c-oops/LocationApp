88406cfdbbb4bf556e46cc68b5d46b3ce6208d6e
Sahil Singh <singhsittu430@gmail.com>
2021-08-01 23:51:04 +0530

    cmd merge change 1

--- app/src/main/java/com/c2c/locationapp/MainActivity.java
+++ app/src/main/java/com/c2c/locationapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
         String s = "1st add";
         String s1 ="stash add";
-        String s2 = "change 2";
+        String s2 = "change 3";
         String s3 = "new change for cmd";
     }
 }
